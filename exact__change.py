5fdac06c79cfe4a881d89237468607f6ee50a50e
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2020-07-23 09:58:45 -0600

    Update exact__change.py

--- exact__change.py
+++ exact__change.py
@@ -3,13 +3,9 @@ amount_of_change = {"dollar" : 0, "quarter" : 0, "dime" : 0, "nickel" : 0, "penn
 
 amount_to_convert = int(input())
 
-if(amount_to_convert <= 0):
-    print("No change")
-
 for change_type in change.keys():
     amount_of_change[change_type] = amount_to_convert // change[change_type]
     amount_to_convert %= change[change_type]
-
 
 else:
     for change_type in amount_of_change.keys():
@@ -21,3 +17,6 @@ else:
                     print(str(amount_of_change[change_type]) + " " + "Pennies")
                 else:
                     print(str(amount_of_change[change_type]) + " " + change_type.title() + "s")
+        else:
+            print("No change")
+            break
